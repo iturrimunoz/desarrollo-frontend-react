648c892d18a200e0fd05c1adea13709cae8c897f
Rodrigo Iturri <iturri@MacBook-Pro-de-Rodrigo.local>
2025-01-08 20:50:10 -0400

    Componentspart 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-
 import './App.css'
-import Default from './Components/default'
+import Logo from './Components/Home/Logo.jsx';
+import Body from './Components/Home/Body.jsx';
+import Footer from './Components/Home/Footer.jsx';
+
+
 
 function App() {
 
-
   return (
     <>
-     <Default/>
+  <Logo/>
+  <Body/>
+  <Footer/>
     </>
   )
 }
-
 export default App
---
+++ src/Components/Home/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+return (
+    <div>
+ <p className="read-the-docs">
+    Click on the Vite and React logos to learn more 
+  </p>
+  </div>
+  );
+};
+
+export default Footer;
