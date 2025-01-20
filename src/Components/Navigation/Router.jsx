5ae1b8bbfbcbd14ed57c5662ca9f03cebe2a26dc
iturrimunoz <iturri@MacBook-Pro-de-Rodrigo.local>
2025-01-19 20:00:40 -0400

    Actualizacion Login

--- src/Components/Navigation/Router.jsx
+++ src/Components/Navigation/Router.jsx
@@ -3,6 +3,7 @@ import About from '../../screens/About.jsx'
 import Think from '../../screens/Think.jsx'
 import Product from '../../screens/Product.jsx'
 import Counter from '../Counter/Counter.jsx'
+import Login from '../../screens/Login.jsx'
 
 import { Routes, Route } from 'react-router'
 
@@ -10,6 +11,7 @@ const Router = () => {
     return (
         <Routes>
            <Route index element={<Home />} />
+           <Route path="login" element={<Login/>}></Route>
            <Route path="product" element={<Product />} />
            <Route path="counter" element={<Counter />} />
            <Route path="think" element={<Think />} />
